632e92a465db818bfac0b68d84d40a0675513939
Ebiperre Iyoro <81704048+Ebiperre@users.noreply.github.com>
2023-02-08 13:32:22 +0100

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyD-HXwiLJxEGacsqEPIZeSt-3mQnwrgElY",
+      authDomain: "auth-216a2.firebaseapp.com",
+      databaseURL: "https://auth-216a2-default-rtdb.firebaseio.com",
+      projectId: "auth-216a2",
+      storageBucket: "auth-216a2.appspot.com",
+      messagingSenderId: "87211857074",
+      appId: "1:87211857074:web:8a24152ccdfdcae91d65ab",
+      measurementId: "G-JTJH3EY89V"
+    }
+  };
